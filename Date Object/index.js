e40d427fa9179973a9db22a84797cada3b71fa46
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-01-04 15:58:54 +0530

    Date Object

---
+++ Date Object/index.js
@@ -0,0 +1,34 @@
+
+// Date(year,month,day,hour,minute,second,ms)
+// const date = new Date(2024,0,4,15,41,5,6)
+// const date = new Date('2024-01-04T03:43')
+
+// const date = new Date()
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDay() == 4 && 'Thu');
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+
+
+// const date = new Date()
+
+// date.setFullYear(2025)
+// date.setMonth(1)
+// date.setDate(6)
+// 
+// 
+// console.log(date);
+
+
+const date1 = new Date('2024-12-31')
+const date2 = new Date('2025-01-1')
+
+
+if (date2 > date1){
+    console.log('Happy new year🥳');
+}
